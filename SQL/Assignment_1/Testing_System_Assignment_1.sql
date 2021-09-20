ca11bfd64e4846bf4a63918f1328701323e71be7
tiennnc96 <tiennnc96@gmail.com>
2021-09-20 16:03:01 +0700

    Assignment 4

--- SQL/Assignment_1/Testing_System_Assignment_1.sql
+++ SQL/Assignment_1/Testing_System_Assignment_1.sql
@@ -91,7 +91,7 @@ CREATE TABLE `Exam` (
     `Code` 			VARCHAR(10) NOT NULL,
     `Title` 		VARCHAR(255) NOT NULL,
     `CategoryID` 	TINYINT NOT NULL,
-    `Duration` 		TIME NOT NULL,
+    `Duration` 		TINYINT NOT NULL,
     `CreatorID` 	TINYINT NOT NULL,
     `CreateDate` 	DATETIME NOT NULL,
     CONSTRAINT fk_Categoryquestion2 FOREIGN KEY (`CategoryID`)
@@ -122,9 +122,12 @@ VALUE ('Phong Ky Thuat 1'),
        ('Phong Tai Chinh');
        
        
-alter table `position` modify column `PositionName` enum ('Dev1', 'Dev2', 'PM', 'Leader', 'Scrum Master' , 'Test');
+alter table `position` modify column `PositionName` enum ('Dev1', 'Dev2', 'PM', 'Leader', 'Scrum Master' , 'Test', 'Developer');
 INSERT INTO `position`(`PositionName`)
-VALUE ('Dev1'),('Dev2'),('PM'),('Scrum Master'),('Test');
+VALUE ('Dev1'),('Dev2'),('PM'),('Scrum Master'),('Test') ,('Developer');
+INSERT INTO `position`(`PositionName`)
+VALUE ('Developer');
+
 
 INSERT INTO `Account`(`Email`, `Username`, `Fullname`, `DepartmentID`, `PositionID`, `CreateDate`)
 VALUES ('tiennnc96@gmail.com', 'vti1', 'Nguyen ngoc tien', 1, 1, '2021-05-20'),
@@ -214,7 +217,7 @@ VALUES ('MS_01', 'De thi 01', 1, 90, 1, NOW()),
        ('MS_05', 'De thi 05', 1, 60, 2, NOW()),
        ('MS_06', 'De thi 06', 2, 90, 2, NOW()),
        ('MS_07', 'De thi 07', 1, 60, 1, NOW());
-       
+SELECT * FROM exam;
 INSERT INTO `ExamQuestion`
 VALUES (1, 1),
        (2, 1),
@@ -354,12 +357,13 @@ VALUES ('MS_08', 'De thi 08', 1, 90, 1, '2021-08-01'),
        ('MS_13', 'De thi 13', 1, 60, 2, '2021-04-01'),
        ('MS_14', 'De thi 14', 2, 90, 2, '2021-03-15'),
        ('MS_15', 'De thi 15', 1, 60, 1, '2021-06-19');
+
 INSERT INTO `Exam`(`Code`, `Title`, `CategoryID`, `Duration`, `CreatorID`, `CreateDate`)
-VALUES 	('MS_16', 'De thi 08', 1, 90, 1, '2019-08-01'),
-		('MS_17', 'De thi 08', 1, 90, 1, '2019-09-01'),
-		('MS_18', 'De thi 08', 1, 90, 1, '2019-10-01'),
-		('MS_19', 'De thi 08', 1, 90, 1, '2019-11-01'); 
-        
+VALUES 	('MS_16', 'De thi 16', 1, 90, 1, '2019-08-01'),
+		('MS_17', 'De thi 17', 1, 90, 1, '2019-09-01'),
+		('MS_18', 'De thi 18', 1, 90, 1, '2019-10-01'),
+		('MS_19', 'De thi 19', 1, 90, 1, '2019-11-01'); 
+	
  /* Question 2: lấy ra tất cả các phòng ban */      
 SELECT DepartmentName FROM department;
 
@@ -417,3 +421,162 @@ UPDATE `account` SET `FullName` = 'Nguyễn Bá Lộc' , `Email` = 'loc.nguyenba
 /* Question 15: update account có id = 5 sẽ thuộc group có id = 4 */
 SELECT * FROM groupaccount;
 UPDATE `groupaccount` SET `GroupID` = 4 WHERE `AccountID` = 5;
+
+/* INNER JOIN */
+SELECT * FROM `account` a JOIN `department` d ON a.DepartmentID = d.DepartmentID;
+SELECT * FROM `account` a JOIN `department` d USING(DepartmentID); -- KHI 2 CỘT CỦA 2 BẢNG GIỐNG NHAU THÌ DÙNG USING
+
+
+
+	/* SQL – Assignment 4 */ 
+    
+/* Question 1: Viết lệnh để lấy ra danh sách nhân viên và thông tin phòng ban của họ */
+SELECT * FROM `account` AS a LEFT JOIN `department` AS d ON a.DepartmentID = d.DepartmentID;
+
+/* Question 2: Viết lệnh để lấy ra thông tin các account được tạo sau ngày 01/12/2019 */
+SELECT * FROM `account` AS a LEFT JOIN `department` AS d ON a.DepartmentID = d.DepartmentID WHERE CreateDate > '2019-12-01';
+
+/* Question 3: Viết lệnh để lấy ra tất cả các Developer */
+SELECT * FROM position;
+SELECT * FROM `account` AS a LEFT JOIN `position` AS d ON a.PositionID = d.PositionID  WHERE PositionName LIKE 'Dev%' ;
+/* Question 4: Viết lệnh để lấy ra danh sách các phòng ban có >3 nhân viên */
+SELECT 
+    *, COUNT(a.`DepartmentID`) AS SLNV
+FROM
+    `account` AS a
+        JOIN
+    `department` AS d ON a.DepartmentID = d.DepartmentID
+GROUP BY a.DepartmentID
+HAVING SLNV > 3;
+
+
+/* Question 5: Viết lệnh để lấy ra danh sách câu hỏi được sử dụng trong đề thi nhiều nhất */
+SELECT q.QuestionID, q.Content, COUNT(Content)  AS TotalQues
+FROM `examquestion` AS e JOIN `question` AS q ON e.QuestionID = q.QuestionID
+GROUP BY e.QuestionID
+HAVING COUNT(e.QuestionID) = 
+	( SELECT MAX(countQues) 
+		FROM ( SELECT COUNT(e.QuestionID) AS countQues
+				FROM examquestion AS e
+				GROUP BY e.QuestionID) AS MaxTotalQues );
+
+/* Question 6: Thông kê mỗi category Question được sử dụng trong bao nhiêu Question */
+SELECT q.CategoryID, c.CategoryName, COUNT(q.CategoryID) AS TotalQues FROM `question` as q INNER JOIN `categoryquestion` as c on q.CategoryID = c.CategoryID
+GROUP BY q.CategoryID;
+
+/* Question 7: Thông kê mỗi Question được sử dụng trong bao nhiêu Exam*/
+SELECT q.Content,COUNT(eq.QuestionID) AS TotalExam FROM `question` AS q LEFT JOIN `examquestion` AS eq ON q.QuestionID = eq.QuestionID
+GROUP BY q.QuestionID;
+
+
+/* Question 8: Lấy ra Question có nhiều câu trả lời nhất */  
+SELECT q.QuestionID, q.Content, COUNT(a.QuestionID) AS Count FROM answer AS a INNER JOIN question AS q ON a.QuestionID = q.QuestionID 
+GROUP BY a.QuestionID 
+HAVING COUNT(a.QuestionID) = (SELECT MAX(CountQues) FROM
+									(SELECT COUNT(a.QuestionID) AS CountQues FROM answer AS a
+										GROUP BY a.QuestionID) AS MaxTotalQues); 
+
+/* Question 9: Thống kê số lượng account trong mỗi group */
+SELECT g.GroupName,ga.GroupID,COUNT(ga.AccountID) AS TotalAcc  FROM `group` AS g INNER JOIN `groupaccount` AS ga ON g.GroupID = ga.GroupID 
+GROUP BY ga.GroupID;
+
+/* Question 10: Tìm chức vụ có ít người nhất*/
+SELECT P.PositionID, P.PositionName, count( A.PositionID) AS SL FROM `account` A
+INNER JOIN `position` P ON A.PositionID = P.PositionID
+GROUP BY A.PositionID
+HAVING count(A.PositionID)= (SELECT MIN(minP) FROM(
+SELECT count(B.PositionID) AS minP FROM `account` B
+GROUP BY B.PositionID) AS MinTatolPosi);
+
+/* Question 11: Thống kê mỗi phòng ban có bao nhiêu dev, test, scrum master, PM */
+ -- SELECT DepartmentID, PositionID , COUNT(*) AS SL FROM `account`
+     -- GROUP BY DepartmentID,PositionID ORDER BY DepartmentID, PositionID;
+
+SELECT   t.DepartmentName, t.PositionName, COUNT(UserName) AS `SL`
+	FROM  `account` a RIGHT JOIN (SELECT * FROM department CROSS JOIN position) AS `t` 
+    ON (a.DepartmentID = t.DepartmentID AND a.PositionID = t.PositionID)
+GROUP BY t.DepartmentID , t.PositionID
+ORDER BY t.DepartmentID , t.PositionID;
+
+/* Question 12: Lấy thông tin chi tiết của câu hỏi bao gồm: thông tin cơ bản của
+question, loại câu hỏi, ai là người tạo ra câu hỏi, câu trả lời là gì, … */
+SELECT q.QuestionID, q.Content, a.FullName, tq.TypeName AS Author, ans.Content FROM question AS q 
+INNER JOIN categoryquestion AS cq ON q.CategoryID = cq.CategoryID
+INNER JOIN typequestion tq ON q.TypeID = tq.TypeID
+INNER JOIN `account` a ON a.AccountID = q.CreatorID
+INNER JOIN Answer AS ans ON q.QuestionID = ans.QuestionID
+ORDER BY q.QuestionID ASC;
+
+
+/* --  Question 13: Lấy ra số lượng câu hỏi của mỗi loại tự luận hay trắc nghiệm */ 
+
+SELECT tq.TypeID, tq.TypeName, COUNT(q.TypeID) AS SL FROM question q
+INNER JOIN typequestion tq ON q.TypeID = tq.TypeID
+GROUP BY q.TypeID; 
+
+/* Question 14:Lấy ra group không có account nào */
+SELECT * FROM `group` g
+LEFT JOIN groupaccount ga ON g.GroupID = ga.GroupID
+WHERE ga.AccountID IS NULL; 
+
+/* Question 15: Lấy ra account không có group nào */ 
+SELECT * FROM groupaccount;
+SELECT * FROM `group` g RIGHT JOIN groupaccount ga ON g.GroupID = ga.GroupID WHERE ga.AccountID IS NULL;
+
+/* Question 16: Lấy ra question không có answer nào*/
+SELECT q.QuestionID FROM answer a
+RIGHT JOIN question q on a.QuestionID = q.QuestionID
+WHERE a.AnswerID IS NULL;
+
+
+/* Question 17:
+a) Lấy các account thuộc nhóm thứ 1 */
+SELECT * FROM groupaccount ;
+SELECT a.AccountID, a.FullName FROM `account` a 
+INNER JOIN `groupaccount` ga on a.AccountID = ga.AccountID 
+INNER JOIN `group` g on ga.GroupID =g.GroupID 
+WHERE ga.GroupID = 1;
+
+/*b) Lấy các account thuộc nhóm thứ 2*/ 
+SELECT a.AccountID, a.FullName FROM `Account` a
+JOIN `groupaccount` ga ON a.AccountID = ga.AccountID
+WHERE ga.GroupID = 2;
+/*c) Ghép 2 kết quả từ câu a) và câu b) sao cho không có record nào trùng nhau*/
+SELECT a.AccountID, a.FullName FROM `account` a 
+INNER JOIN `groupaccount` ga on a.AccountID = ga.AccountID 
+INNER JOIN `group` g on ga.GroupID =g.GroupID 
+WHERE ga.GroupID = 1
+UNION
+SELECT a.AccountID, a.FullName FROM `Account` a
+JOIN `groupaccount` ga ON a.AccountID = ga.AccountID
+WHERE ga.GroupID = 2;
+
+/* Question 18:
+/* a) Lấy các group có lớn hơn 5 thành viên*/
+SELECT g.GroupName, COUNT(ga.GroupID) AS SL
+FROM GroupAccount ga
+JOIN `Group` g ON ga.GroupID = g.GroupID
+GROUP BY g.GroupID
+HAVING COUNT(ga.GroupID) >= 5;
+
+/* b) Lấy các group có nhỏ hơn 7 thành viên*/
+SELECT g.GroupName, COUNT(ga.GroupID) AS SL
+FROM GroupAccount ga
+JOIN `Group` g ON ga.GroupID = g.GroupID
+GROUP BY g.GroupID
+HAVING COUNT(ga.GroupID) <= 7;
+
+-- c) Ghép 2 kết quả từ câu a) và câu b)*/
+SELECT g.GroupName, COUNT(ga.GroupID) AS SL
+FROM GroupAccount ga
+JOIN `Group` g ON ga.GroupID = g.GroupID
+GROUP BY g.GroupID
+HAVING COUNT(ga.GroupID) >= 5
+UNION 
+SELECT g.GroupName, COUNT(ga.GroupID) AS SL
+FROM GroupAccount ga
+JOIN `Group` g ON ga.GroupID = g.GroupID
+GROUP BY g.GroupID
+HAVING COUNT(ga.GroupID) <= 7;
+
+/**/
